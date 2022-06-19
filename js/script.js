88511084cd4c2bba7b8d372ac004bcbcfbe8bf07
Evgen <golovachev.e@gmail.com>
2022-06-19 22:12:34 +0300

    films

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log(1);
+const NumberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const PersonalMovieDB = {
+    count : NumberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+b = prompt('На сколько оцените его?', ''),
+c = prompt('Один из последних просмотренных фильмов?', ''),
+d = prompt('На сколько оцените его?', '');
+
+PersonalMovieDB.movies[a] = b;
+PersonalMovieDB.movies[c] = d;
+console.log(PersonalMovieDB);
